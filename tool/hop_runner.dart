cbf5c0a335245a0a7723f5a06d3709dc3a889e56
Damon Douglas <douglas.damon@gmail.com>
2013-12-28 12:39:55 -0500

    Update hop_runner.dart

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -3,14 +3,12 @@ library hop_runner;
 import 'package:hop/hop.dart';
 import 'package:hop/hop_tasks.dart';
 
-void main(List<String> arguments) {
+void main(List<String> args) {
   var paths = ['bin/${1}.dart']; // etc, etc, etc
 
   addTask('docs', createDartDocTask(paths, linkApi: true));
   
   addTask('analyze_libs', createAnalyzerTask(paths));
   
-  addTask('dart2js', createDartCompilerTask(paths, liveTypeAnalysis: true, rejectDeprecatedFeatures: true));
-  
-  runHop();
+  runHop(args);
 }
